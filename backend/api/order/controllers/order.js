4b5fb1dd244d7c2d3e289b6322e89a9bf08994ca
Jason Rahm <jasonrahm@gmail.com>
2023-11-13 15:24:54 -0700

    stripe installed

--- backend/api/order/controllers/order.js
+++ backend/api/order/controllers/order.js
@@ -1,8 +1,40 @@
-'use strict';
+"use strict";
+const stripe = require("stripe")(
+  "sk_test_51OBlYLAKYJ4RdA10U3UaoraVvFF9eXg7qFRNPyEXOSITWJQDnVco9EThVXySin1rGfYwldQuF74HKaI6CdVeJQ7900O5VeeXGS"
+);
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  /**
+   * Create a/an order record.
+   *
+   * @return {Object}
+   */
 
-module.exports = {};
+  create: async (ctx) => {
+    const { address, amount, dishes, token, city, state } = JSON.parse(
+      ctx.request.body
+    );
+    const stripeAmount = Math.floor(amount * 100);
+    // charge on stripe
+    const charge = await stripe.charges.create({
+      // Transform cents to dollars.
+      amount: stripeAmount,
+      currency: "usd",
+      description: `Order ${new Date()} by ${ctx.state.user._id}`,
+      source: token,
+    });
+
+    // Register the order in the database
+    const order = await strapi.services.order.create({
+      user: ctx.state.user.id,
+      charge_id: charge.id,
+      amount: stripeAmount,
+      address,
+      dishes,
+      city,
+      state,
+    });
+
+    return order;
+  },
+};
