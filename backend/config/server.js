582eb85ca54c35fed54b983b79b6a576e32db2ab
Jason Rahm <jasonrahm@gmail.com>
2023-11-20 09:11:08 -0700

    server set to hosted url

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: "http://localhost:1337",
+  url: env("HOSTED_URL"),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "910f5455c13d759942f92aeb79a5b4e3"),
